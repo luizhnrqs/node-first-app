450706c5569d17d733c974bdbe41bea1a7251374
Luiz Henrique da Silva <henriquesilva.luiz@gmail.com>
2018-08-28 01:37:13 -0300

    inclusão de models no projeto

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,10 +1,10 @@
 module.exports = function(app){
-    app.get('/noticias', function(req, res){
+    app.get('/noticia', function(req, res){
         var connection = app.config.dbConnection();
         var noticiasModel = app.app.models.noticiasModel;
 
         noticiasModel.getNoticias(connection, function(error, result){
-            res.render('noticias/noticias', {noticias : result});
-        });        
+            res.render('noticias/noticia', {noticia : result});
+        });  
     });
-};
+}
--- config/server.js
+++ config/server.js
@@ -1,7 +1,14 @@
 var express = require('express');
-var app = express();
+var consign = require('consign');
 
+var app = express();
 app.set('view engine', 'ejs'); // Altera o view engine que o express está utilizando para EJS
 app.set('views', './app/views'); // Altera o diretório de views
+
+consign()   //autoload
+    .include('app/routes')
+    .then('config/dbConnection.js') //necessário apontar a extensão para que o node não se confunda com um diretório
+    .then('app/models') //carrega todas as models presentes na pasta models
+    .into(app); //reconhece todos os arquivos da pasta routes e incluí os módulos dentro do servidor "app"
 
 module.exports = app;
