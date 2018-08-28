6b081b3ba4bf8564f32f21cda40f29d8aa2782e4
Luiz Henrique da Silva <henriquesilva.luiz@gmail.com>
2018-08-28 00:37:23 -0300

    Instalacao do consign

--- config/server.js
+++ config/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+
 app.set('view engine', 'ejs'); // Altera o view engine que o express está utilizando para EJS
 app.set('views', './app/views'); // Altera o diretório de views
+
 module.exports = app;
